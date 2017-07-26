bc3b7da639f03d009aee4afc4e9d05e90ce69ac7
lm <425017146@qq.com>
2017-07-26 19:09:01 +0800

    Update file.js

--- models/file.js
+++ models/file.js
@@ -1,9 +1,6 @@
-//model层文件读取
+//model层
 var fs = require("fs");
-//由于异步读取文件,controller层填充模板在读取结果前
-//所以填充模板没有数据,通过回调函数解决
 exports.getDirNames = function(callback){
-    //读取文件夹下所有内容
     fs.readdir("./uploads",function(err,data){
         var dirNames = [];
         //判断是否为文件夹fs.stat异步,通过迭代器解决异步问题
@@ -12,10 +9,8 @@ exports.getDirNames = function(callback){
                 callback(dirNames);
                 return;
             }
-            //判断是否为文件夹
             fs.stat("./uploads/"+data[i],function(err,stats){
                 if(stats.isDirectory()) dirNames.push(data[i]);
-                //在异步完成后再处理下一个,所以迭代位置在异步判断内
                 iterator(i+1);
             });
         })(0);
@@ -29,10 +24,8 @@ exports.getPhotosByAlbum = function(album,callback){
             return;
         }
         var files = [];
-        //判断是否为文件
         (function iteration(i){
             if(i == data.length){
-                //这里的null就是router.js回调函数的实参中err
                 callback(null,files);
                 return;
             }
